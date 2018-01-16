07db9e00bb1cf8497547fb34794409f93e5c9f72
leviathan <farzad.u235@gmail.com>
2018-01-16 04:20:33 +0330

    fuckkkkkkkdfgksss

--- resources/views/coach/dashboard.blade.php
+++ resources/views/coach/dashboard.blade.php
@@ -63,7 +63,7 @@
 
         $(".j-remove-swimmer-from-team").click(function () {
             $("#remove-modal").fadeIn();
-            var a = $(this).closest(".j-swimmer-id").innerHTML();
+            var a = $(this).closest(".j-swimmer-id").innerHTML;
             alert(a);
         });
 
